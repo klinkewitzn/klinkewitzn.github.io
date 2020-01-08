035d2895af3d992c5a09c58cf8bbc7366d12728c
Nadine Klinkewitz <klinkewitzn@web.de>
2020-01-08 17:39:20 +0100

    aufgabe9 update

--- aufgabe_9/toDoList.ts
+++ aufgabe_9/toDoList.ts
@@ -1,49 +1,46 @@
-var eintraege: string[] = ["einkaufen gehen", "Familie besuchen", "weiteres"];
-
-
-
-
-
-window.addEventListener("load", function(): void {
-
-    list();
-
-    
-    document.querySelector("#addtask").addEventListener("click", function(): void {
-
-        var input: HTMLInputElement = document.querySelector("#input");
-        eintraege.push(input.value);
-        input.value = "";
-        console.log("huhu");
-        list();
-
-    });
-
-    function list(): void {
-        document.querySelector("#eintraege").innerHTML = "";
-        for (var index: number = 0; index < eintraege.length; index++){
-            document.querySelector("#eintraege").innerHTML += "<p>" + "<i class='far fa-circle'></i>" + eintraege[index]+"</p>";
-        }
-        document.querySelector("#tasknumber").innerHTML = "" + eintraege.length;
-    
-    }
-    
+var index = 0;
+window.addEventListener("load", function () {
+    document.querySelector(".addButton").addEventListener("click", AddTaskText);
+    document.querySelector(".items").addEventListener("click", AddTaskText);
 });
-/* 
-document.querySelector("#eintraege").innerHTML = "";
-in Schleife
-document.querySelector(".elemt:last-child fa-check").addEventListener("click", )
-
-var element: HTMLElement;
-element=document.querySelector("#content");
-var new=document.createElement("div");
-new.id="bla";
-element.appendChild(new);
------
-button.name="PushMe"
-console.log(element.name)
-------
-handler
-html element
-JS HTML DOM!!!! W3 schools
- */
+function AddTaskText() {
+    var newtask = document.querySelector(".newtask").value;
+    console.log(newtask);
+    addTaskToList(newtask);
+    var element = document.querySelector(".Rahmen");
+    var numberofChildren = element.children.length;
+    index = numberofChildren;
+    console.log("Die Anzahl der To-Dos lautet:" + numberofChildren);
+    document.querySelector(".items").innerHTML = numberofChildren + " in total";
+}
+function addTaskToList(newTaskValue) {
+    var addElement = document.createElement("div");
+    addElement.setAttribute("class", "Template");
+    addElement.setAttribute("id", "TaskId" + index); // Template konstruiert
+    var myHtmlTemplate = ""; // Template Inhalte
+    myHtmlTemplate += "<button onclick=\"CheckedButton(this)\" class=\"far fa-circle checkbox\" id=\"circle" + index + "\"></button>";
+    myHtmlTemplate += "<input class=\"firsttask\" id=\"firsttask" + index + "\" value=\"" + newTaskValue + "\" ></input>";
+    myHtmlTemplate += "<button onclick=\"DeleteTaskText(this)\" class=\"far fa-trash-alt trash\" id=\"delete" + index + "\"></button>";
+    addElement.innerHTML = myHtmlTemplate;
+    document.querySelector(".Rahmen").appendChild(addElement); // Hinzufügen vom Template
+    console.log("addTasktoList:" + newTaskValue);
+}
+function DeleteTaskText(ClickedTrash) {
+    console.log("This is deleating the task");
+    ClickedTrash.parentElement.remove(); // div wird bei click auf Tonne gelöscht
+    index--;
+    // Counter
+    var element = document.querySelector(".Rahmen");
+    var numberofChildren = element.children.length;
+    document.querySelector(".items").innerHTML = numberofChildren + " in total";
+}
+function CheckedButton(ClickedButton) {
+    if (ClickedButton.classList.contains("fa-circle")) {
+        ClickedButton.classList.remove("fa-circle");
+        ClickedButton.classList.add("fa-check-circle");
+    }
+    else {
+        ClickedButton.classList.remove("fa-check-circle");
+        ClickedButton.classList.add("fa-circle");
+    }
+}
---
+++ test/A9/aufgabe9.js
@@ -0,0 +1,168 @@
+var eintraege = ["einkaufen gehen", "Familie besuchen", "weiteres"];
+window.addEventListener("load", function () {
+    list();
+    document.querySelector("#addTask").addEventListener("click", function () {
+        var input = document.querySelector("#input");
+        eintraege.push(input.value);
+        input.value = "";
+        console.log("huhu");
+        list();
+    });
+    function list() {
+        document.querySelector("#eintraege").innerHTML = "";
+        for (var index = 0; index < eintraege.length; index++) {
+            document.querySelector("#eintraege").innerHTML += "<p class=activity >" + "<i class='far fa-circle checkbox'></i>" + "  " + eintraege[index] + "</input>" + "<button onclick=\"Remove(this)\" class='far fa-trash-alt trash'>" + "</button>" + "     " + "</p>" + "<br>";
+        }
+        document.querySelector("#tasknumber").innerHTML = "" + eintraege.length;
+    }
+    function Remove(ClickedTrash) {
+        console.log("This is deleating the task");
+        ClickedTrash.parentElement.remove(); // p wird bei click auf Tonne gelöscht
+        index--;
+        /*
+                function CheckedButton(ClickedButton) {
+                    if (ClickedButton.classList.contains("fa-circle")) {
+                        ClickedButton.classList.remove("fa-circle");
+                        ClickedButton.classList.add("fa-check-circle");
+                    }
+                    else {
+                        ClickedButton.classList.remove("fa-check-circle");
+                        ClickedButton.classList.add("fa-circle");
+                    }
+                }
+            }
+        
+        }
+            function DeleteTaskText(ClickedTrash) {
+                console.log("This is deleating the task");
+                ClickedTrash.parentElement.remove(); // div wird bei click auf Tonne gelöscht
+                taskcounter--;
+                // Counter
+                var element = document.querySelector(".Rahmen");
+                var numberofChildren = element.children.length;
+                document.querySelector(".items").innerHTML = numberofChildren + " in total";
+            }
+        
+        
+            // Create a "close" button and append it to each list item
+            var myNodelist = document.querySelector("#eintraege");
+        var index;
+        for (index = 0; index < myNodelist.length; index++) {
+            var span = document.createElement("SPAN");
+            var txt = document.createTextNode("\u00D7");
+            span.className = "close";
+            span.appendChild(txt);
+            myNodelist[i].appendChild(span);
+        }
+        
+        // Click on a close button to hide the current list item
+        var close = document.getElementsByClassName("close");
+        var i;
+        for (i = 0; i < close.length; i++) {
+            close[i].onclick = function () {
+                var div = this.parentElement;
+                div.style.display = "none";
+            }
+        }
+        
+        });
+        */
+    }
+    /*
+            function CheckedButton(ClickedButton) {
+                if (ClickedButton.classList.contains("fa-circle")) {
+                    ClickedButton.classList.remove("fa-circle");
+                    ClickedButton.classList.add("fa-check-circle");
+                }
+                else {
+                    ClickedButton.classList.remove("fa-check-circle");
+                    ClickedButton.classList.add("fa-circle");
+                }
+            }
+        }
+    
+    }
+        function DeleteTaskText(ClickedTrash) {
+            console.log("This is deleating the task");
+            ClickedTrash.parentElement.remove(); // div wird bei click auf Tonne gelöscht
+            taskcounter--;
+            // Counter
+            var element = document.querySelector(".Rahmen");
+            var numberofChildren = element.children.length;
+            document.querySelector(".items").innerHTML = numberofChildren + " in total";
+        }
+    
+    
+        // Create a "close" button and append it to each list item
+        var myNodelist = document.querySelector("#eintraege");
+    var index;
+    for (index = 0; index < myNodelist.length; index++) {
+        var span = document.createElement("SPAN");
+        var txt = document.createTextNode("\u00D7");
+        span.className = "close";
+        span.appendChild(txt);
+        myNodelist[i].appendChild(span);
+    }
+    
+    // Click on a close button to hide the current list item
+    var close = document.getElementsByClassName("close");
+    var i;
+    for (i = 0; i < close.length; i++) {
+        close[i].onclick = function () {
+            var div = this.parentElement;
+            div.style.display = "none";
+        }
+    }
+    
+    });
+    */
+});
+/*
+        function CheckedButton(ClickedButton) {
+            if (ClickedButton.classList.contains("fa-circle")) {
+                ClickedButton.classList.remove("fa-circle");
+                ClickedButton.classList.add("fa-check-circle");
+            }
+            else {
+                ClickedButton.classList.remove("fa-check-circle");
+                ClickedButton.classList.add("fa-circle");
+            }
+        }
+    }
+
+}
+    function DeleteTaskText(ClickedTrash) {
+        console.log("This is deleating the task");
+        ClickedTrash.parentElement.remove(); // div wird bei click auf Tonne gelöscht
+        taskcounter--;
+        // Counter
+        var element = document.querySelector(".Rahmen");
+        var numberofChildren = element.children.length;
+        document.querySelector(".items").innerHTML = numberofChildren + " in total";
+    }
+
+
+    // Create a "close" button and append it to each list item
+    var myNodelist = document.querySelector("#eintraege");
+var index;
+for (index = 0; index < myNodelist.length; index++) {
+    var span = document.createElement("SPAN");
+    var txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    myNodelist[i].appendChild(span);
+}
+
+// Click on a close button to hide the current list item
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+    close[i].onclick = function () {
+        var div = this.parentElement;
+        div.style.display = "none";
+    }
+}
+
+});
+*/
+//# sourceMappingURL=aufgabe9.js.map
